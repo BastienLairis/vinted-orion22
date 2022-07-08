757bf83c8d2315f7f8813b95e1879221ecd2d665
bastienlairis <bastien.lairis@gmail.com>
2022-07-08 11:39:40 +0200

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 //connexion à la bdd
 // mongoose.connect("mongodb://localhost/vinted-orion22");
 mongoose.connect(process.env.DATABASE_URL);
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
